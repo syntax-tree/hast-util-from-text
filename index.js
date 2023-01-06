ebb806a0fb13cf14774475673479d4a159f0ff84
Titus Wormer <tituswormer@gmail.com>
2023-01-06 12:40:06 +0100

    Refactor code-style
    *   Accept `null` as input in types
    *   Add more docs to JSDoc
    *   Refactor for improved scoping of variables

--- index.js
+++ index.js
@@ -1,30 +1,46 @@
 /**
- * @typedef {import('hast').Parent['children'][number]|import('hast').Root} HastNode
- * @typedef {import('hast').Text} HastText
- * @typedef {import('hast').Element & {tagName: 'br'}} HastBreakElement
+ * @typedef {import('hast').Root} Root
+ * @typedef {import('hast').Content} Content
+ * @typedef {import('hast').Text} Text
+ * @typedef {import('hast').Element} Element
+ */
+
+/**
+ * @typedef {Content | Root} Node
+ * @typedef {Element & {tagName: 'br'}} BreakElement
  */
 
 const search = /\r?\n|\r/g
 
+// To do next major: do not return given element.
 /**
+ * Set the plain-text value of a node.
+ *
  * Implementation of the `innerText` setter:
  * <https://html.spec.whatwg.org/#the-innertext-idl-attribute>
- * Note that `innerText` only exists on element.
- * In this utility, we accept all parent nodes and handle them as elements, and
- * for all literals we set the `value` of the given node the the given value.
  *
- * @template {HastNode} T
+ * > 👉 **Note**: `innerText` only exists on elements.
+ * > In this utility, we accept all parent nodes and handle them as elements,
+ * > and for all literals we set the `value` of the given node the the given
+ * > value.
+ *
+ * @template {Node} T
+ *   Node type.
  * @param {T} node
- * @param {string} [content]
+ *   Tree to change.
+ * @param {string | null | undefined} [text]
+ *   Value to set.
  * @returns {T}
+ *   Given, modified, tree.
  */
-export function fromText(node, content) {
-  const value = content === undefined || content === null ? '' : String(content)
-  /** @type {Array<HastBreakElement|HastText>} */
-  const nodes = []
-  let start = 0
+export function fromText(node, text) {
+  const value = text === undefined || text === null ? '' : String(text)
 
   if ('children' in node) {
+    /** @type {Array<BreakElement | Text>} */
+    const nodes = []
+    let start = 0
+
     while (start < value.length) {
       search.lastIndex = start
       const match = search.exec(value)
